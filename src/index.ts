657a48c61792cd5c2e275db561b2db9ef8ea90e7
pviniciusm <pvinics@gmail.com>
2023-07-24 20:24:46 -0300

    refactor: mudança da configuração e listen do express para a main

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
-import cors from "cors";
-import express, { Request, Response } from "express";
-import { appRoutes } from "./routes/user.routes";
-import * as dotenv from "dotenv";
+import { Server } from "./main/config/express.config";
 import { Database } from "./main/database/database.connection";
 import "reflect-metadata";
-
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use("/users", appRoutes());
 
 Database.connect().then(() => {
     console.log("Database is connected!");
 
-    app.listen(process.env.PORT, () => {
-        console.log("Servidor rodando na porta " + process.env.PORT + "!");
-    });
+    const app = Server.create();
+    Server.listen(app);
 });
---
+++ src/main/config/express.config.ts
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express, { Request, Response, Express } from "express";
+import { appRoutes } from "../../routes/user.routes";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export class Server {
+    public static create() {
+        const app = express();
+        app.use(express.json());
+        app.use(cors());
+
+        app.use("/users", appRoutes());
+
+        return app;
+    }
+
+    public static listen(app: Express) {
+        app.listen(process.env.PORT, () => {
+            console.log("Servidor rodando na porta " + process.env.PORT + "!");
+        });
+    }
+}
