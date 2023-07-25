82306334784f8730051670c3d349aafb874e9d72
pviniciusm <pvinics@gmail.com>
2023-07-24 22:42:09 -0300

    feat: adapter para retornos de usecases

---
+++ src/app/shared/util/return.adapter.ts
@@ -0,0 +1,28 @@
+import { Result } from "../contracts/result.contract";
+
+export class Return {
+    public static invalidCredentials(): Result {
+        return {
+            ok: false,
+            message: "Acesso não autorizado",
+            code: 401,
+        };
+    }
+
+    public static success(message: string, data: any): Result {
+        return {
+            ok: true,
+            message,
+            data,
+            code: 200,
+        };
+    }
+
+    public static notFound(entity: string): Result {
+        return {
+            ok: false,
+            message: `${entity} not found`,
+            code: 404,
+        };
+    }
+}
